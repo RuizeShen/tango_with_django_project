60c1ddaf841d48a46dc1e93d8d956b62d8ba2d4b
Ruize <22361590s@student.gla.ac.uk>
2019-02-02 12:00:45 +0000

    Ch5

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -131,3 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
